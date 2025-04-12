e52793e05c3b4cbeb10b72f5a827544c51644a1f
krishnangi-agraw <krishnangi.agrawal_cs22@gla.ac.in>
2025-04-12 17:53:27 +0530

    minor updated

---
+++ README.md
@@ -0,0 +1,11 @@
+# Weather Dashboard Project
+
+## Description
+This is a real-time weather dashboard built using the MERN stack (MongoDB, Express, React, Node.js). Users can input a city and view live weather data, including temperature, humidity, wind speed, and weather condition.
+
+## Setup Instructions
+
+### 1. Clone the repository
+```bash
+git clone https://github.com/Snehaagraw/Weather-Forecast.git
+cd weather-dashboard
--- client/src/components/WeatherCard.jsx
+++ client/src/components/WeatherCard.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 
 const WeatherCard = ({ data }) => {
     return(
-        <div className="weather-card">
-            <h2>{data?.name}</h2>
-            <img src={`http://openweathermap.org/img/wn/${data.icon}@2x.png`} alt={data.condition} />
-            <p>Temperature:{data.temperature}C</p>
-            <p>Condition:{data.condition}</p>
-            <p>Humidity:{data.humidity}%</p>
-            <p>Wind Speed:{data.windSpeed}m/s</p>
+        <div className="weather-dashboard">
+            <div className="weather-box">
+                <h2>City</h2>
+                <p>{data.name}</p>
+            </div>
+            <div className="weather-box">
+                <h2>Condition</h2>
+                <img src={`http://openweathermap.org/img/wn/${data.icon}@2x.png`} alt="icon" />
+                <p>{data.condition}</p>
+            </div>
+            <div className="weather-box">
+                <h2>Temperature</h2>
+                <p className="temp">{data.temperature}°C</p>
+            </div>
+            <div className="weather-box">
+                <h2>Humidity</h2>
+                <p>{data.humidity}%</p>
+            </div>
+
+            <div className="weather-box">
+                <h2>Wind Speed</h2>
+                <p>{data.windSpeed} km/h</p>
+            </div>
         </div>
     );
 };
